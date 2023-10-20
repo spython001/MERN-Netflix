eebe4a20136dd1cc7ab02033ad559cb04ba7cc76
spython <emmanuelegbo01@gmail.com>
2023-10-20 11:55:58 +0100

    commit

---
+++ api/models/Movie.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const MovieSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    desc:{
+        type:String,
+    },
+    img:{
+        type:String,
+    },
+}, {timestamps:true});
+
+
+module.exports = mongoose.model('Movie', MovieSchema);
