3a72b92e2b9aefb49d51e32ef949b3425608de8d
spython <emmanuelegbo01@gmail.com>
2023-10-21 09:39:42 +0100

    modified contents of auth js route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async(req,res)=>{
+    //CREATING HASHED PASSWORD WITH BCRYPT
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
     });
     try {
         const user = await newUser.save();
@@ -18,6 +24,27 @@ router.post("/register", async(req,res)=>{
 
 //LOGIN
 router.post("/login", async(req,res) =>{
+    try{
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status(401).json('Wrong email or password');
+
+        //VALIDATING PASSWORD FOR UNIQUE USER
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        !validated && res.status(400).json("Wrong email or password");
+
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },  process.env.JWT_SEC,
+            { expiresIn: "5d" }
+        );
+
+        const {password, ...info} = user._doc;
+
+        res.status(201).json({ ...info, accessToken });
+    }catch(err){
+        res.status(500).json(err);
+    }
     
 })
 
